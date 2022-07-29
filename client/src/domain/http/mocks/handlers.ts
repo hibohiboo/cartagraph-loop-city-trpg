1684abf0842fe0d45de8c73bb528090d2366ae7b
hibohiboo <swdice66@gmail.com>
2022-07-29 23:21:49 +0900

    fix mock user

--- client/src/domain/http/mocks/handlers.ts
+++ client/src/domain/http/mocks/handlers.ts
@@ -3,5 +3,7 @@ export const handlers = [
   rest.put('/v1/api/solo-journal/scenario/:uuid', (req, res, ctx) => {
     return res(ctx.status(200))
   }),
-  rest.get('/user', null),
+  rest.get('/user', (req, res, ctx) => {
+    return res(ctx.status(200))
+  }),
 ]
