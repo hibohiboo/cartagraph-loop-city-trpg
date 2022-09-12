5847755fac4e6bdde77eabc4eff55ae5eb4e85db
hibohiboo <swdice66@gmail.com>
2022-09-12 21:42:59 +0900

    add 幕間 AB#204

--- client/src/components/pages/BoogieAnalytics.tsx
+++ client/src/components/pages/BoogieAnalytics.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Wrapper from '@/components/atoms/app/Wrapper'
 import PlaceCardFront from '@/card/PlaceCard'
+import SceneCardFront from '@/card/SceneCard'
 
 const BoogieAnalytics: React.FC = () => {
   return (
@@ -8,9 +9,27 @@ const BoogieAnalytics: React.FC = () => {
       <h1>ブギーポップ因数分解</h1>
       <section>
         <h2>イントロダクション Introduction</h2>
-        <PlaceCardFront />
+        <div style={{ display: 'flex' }}>
+          <SceneCardFront card={sceneIntroduction} />
+          <PlaceCardFront />
+        </div>
       </section>
     </Wrapper>
   )
 }
 export default BoogieAnalytics
+
+const sceneIntroduction = {
+  id: 0,
+  name: 'Introduction',
+  nameRuby: 'イントロダクション',
+  keywords: ['事件', '邂逅'],
+  timing: '幕間',
+  location: '茶室',
+  effect: `マスターシーンを開始する。
+物語の始まりの予感を演出したら、シーンを終了する`,
+  flavor: `茶室の真ん中に黒髪の少女の死体があるのを少年は発見する。
+「見たな」
+天井に張り付いた殺戮者。男でも女でもない生き物が、少年に跳びかかった。
+`,
+}
